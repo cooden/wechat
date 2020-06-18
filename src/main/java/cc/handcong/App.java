ad5b810421adf8c66d2a64f957f769ecc0dd5ade
cooden <434475383@qq.com>
2020-06-18 09:25:09 +0800

    修改controller

---
+++ src/main/java/cc/handcong/App.java
@@ -0,0 +1,17 @@
+package cc.handcong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableAutoConfiguration
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/cc/handcong/controller/controller.java
@@ -0,0 +1,18 @@
+package cc.handcong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+    @RequestMapping("/")
+    public String index(){
+        return "/index.html";
+    }
+
+    @RequestMapping("/realTime")
+    public String echart(){
+        return "/realTime.html";
+    }
+
+}
